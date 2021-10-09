6e14ec853cf0bc3e538ce5171061a3387a4ae961
Darina Kopenkova <darisha_92@mail.ru>
2021-10-09 12:10:17 +0300

    use axios to fetch data

--- src/App.js
+++ src/App.js
@@ -3,38 +3,25 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { ShipmentList } from "./pages/ShipmentList/ShipmentList";
 import { ShipmentDetails } from "./pages/ShipmentDetails/ShipmentDetails";
-
-const shipments = [
-  {
-    orderNo: "wd-188849-82159474-8070250",
-    date: "2/17/2019",
-    customer: "Wolverine World Wide, Inc.",
-    trackingNo: "TP-353069-93442245-9242241",
-    status: "'Delivered'",
-    consignee: "Comcast Corporation",
-  },
-  {
-    orderNo: "eo-587622-81145661-2433957",
-    date: "10/30/2018",
-    customer: "Turkcell Iletisim Hizmetleri AS",
-    trackingNo: "TP-270097-20971314-9825251",
-    status: "'Delivered'",
-    consignee: "TE Connectivity Ltd.",
-  },
-  {
-    orderNo: "ms-869145-96178691-3326235",
-    date: "9/22/2019",
-    customer: "Cimarex Energy Co",
-    trackingNo: "TP-512025-02593091-2625679",
-    status: "'Delivered'",
-    consignee: "Old Second Bancorp, Inc.",
-  },
-];
+import axios from "axios";
 
 function App() {
+  const [list, setList] = React.useState([]);
+  React.useEffect(() => {
+    axios
+      .get("https://my.api.mockaroo.com/shipments.json?key=5e0b62d0")
+      .then(function (response) {
+        setList(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div>
-      <ShipmentList list={shipments} />
+      <ShipmentList list={list} />
       {/* <ShipmentDetails /> */}
     </div>
   );
