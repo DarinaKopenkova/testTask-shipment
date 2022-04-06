a59cbde2b362225721a840ef0d549e5d4708f754
Darina Kopenkova <darisha_92@mail.ru>
2022-04-06 19:14:46 +0300

    added markup to sort items

--- src/pages/ShipmentList/ShipmentList.js
+++ src/pages/ShipmentList/ShipmentList.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Table } from "react-bootstrap";
 import styles from "./ShipmentList.module.css";
 import { ShipmentListItem } from "./components/ShipmentListItem";
+import { ReactComponent as ArrowUp } from "../../components/Arrows/UpArrow.svg";
+import { ReactComponent as ArrowDown } from "../../components/Arrows/DownArrow.svg";
 
 export function ShipmentList(props) {
   return (
@@ -10,10 +12,42 @@ export function ShipmentList(props) {
         <tr className={styles.header}>
           <th>orderNo</th>
           <th>delivery date</th>
-          <th>customer</th>
-          <th>trackingNo</th>
-          <th>status</th>
-          <th>consignee</th>
+          <th>
+            <div className={styles.title}>
+              <div className={styles.title__name}>customer</div>
+              <div className={styles.title__arrows}>
+                <ArrowUp className={styles.arrowUp} />
+                <ArrowDown className={styles.arrowDown} />
+              </div>
+            </div>
+          </th>
+          <th>
+            <div className={styles.title}>
+              <div className={styles.title__name}>trackingNo</div>
+              <div>
+                <ArrowUp className={styles.arrowUp} />
+                <ArrowDown className={styles.arrowDown} />
+              </div>
+            </div>
+          </th>
+          <th>
+            <div className={styles.title}>
+              <div className={styles.title__name}>status</div>
+              <div>
+                <ArrowUp className={styles.arrowUp} />
+                <ArrowDown className={styles.arrowDown} />
+              </div>
+            </div>
+          </th>
+          <th>
+            <div className={styles.title}>
+              <div className={styles.title__name}>consignee</div>
+              <div>
+                <ArrowUp className={styles.arrowUp} />
+                <ArrowDown className={styles.arrowDown} />
+              </div>
+            </div>
+          </th>
           <th></th>
         </tr>
       </thead>
